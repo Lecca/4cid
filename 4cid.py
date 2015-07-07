ce3e582ad02b31e344ee59cabb8c2bb58efa858c
Lecca <Lecca@protonmail.com>
2015-07-07 12:49:22 +0900

    Fixed

---
+++ 4cid.py
@@ -0,0 +1,30 @@
+import urllib.request as urllib, argparse, os, re
+
+def urlget(board, thread):
+	return("https://boards.4chan.org/"+board+"/thread/"+thread)
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("board")
+	parser.add_argument("thread")
+	args = parser.parse_args()
+	wdir = os.path.dirname(os.path.abspath(__file__))
+	ddir = os.path.join(wdir,'downloads',args.board,args.thread)
+	if not os.path.exists(ddir):
+		os.makedirs(ddir)
+	url = urlget(args.board, args.thread)
+	sturl = str(urllib.urlopen(url).read())
+	for _ in re.findall('//i.4cdn.org/\w+/\d*\.\w{3,4}?',sturl):
+		try:
+			imgdir = os.path.join(ddir, _[15:])
+			imglink = 'http:'+_
+			urllib.urlretrieve(imglink, imgdir)
+			print('Downloaded '+imglink)
+		except Exception as e:
+			print(e)
+
+if __name__ == "__main__":
+	try:
+		main()
+	except Exception as e:
+		print(e)
